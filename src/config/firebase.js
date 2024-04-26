07ec2c6dd779550be59443cab101ba366f74e397
Nayakv46 <bartoszjaniak04@gmail.com>
2024-04-26 14:54:41 +0200

    Firebase base config

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJiDSWhyNAj8kOANdKmzTKxN4JKsTK2Ek",
+  authDomain: "talenthub-b2fc8.firebaseapp.com",
+  projectId: "talenthub-b2fc8",
+  storageBucket: "talenthub-b2fc8.appspot.com",
+  messagingSenderId: "196883685981",
+  appId: "1:196883685981:web:02cfdad57ce6081be523b4",
+  measurementId: "G-VCHPZ8602E"
+};
+
+const app = initializeApp(firebaseConfig);
