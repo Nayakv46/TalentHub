22eefbe628284dc81c30bb9e66299983ae700f95
Nayakv46 <bartoszjaniak04@gmail.com>
2024-05-05 21:41:23 +0200

    CandidateCard details popup styling and transitions

--- src/components/EmployerPage/CandidateCard/CandidateCard.jsx
+++ src/components/EmployerPage/CandidateCard/CandidateCard.jsx
@@ -1,12 +1,23 @@
 import './candidateCard.scss';
+import { useState } from 'react';
+import { FaRegEnvelope } from 'react-icons/fa';
+import { IoClose } from 'react-icons/io5';
+import useMountTransition from '../../../utils/useMountTransition';
 
 const CandidateCard = ({ key, email, experience }) => {
+
+    const [showDetails, setShowDetails] = useState(false);
+    const hasTransitionedInDetails = useMountTransition(showDetails, 250);
 
   return (
     <div className='candidate-card' key={key}>
         <p className='candidate-card__email'>
             {email}
         </p>
+
+        <button className='candidate-card__show-details' onClick={() => setShowDetails(!showDetails)}>
+            <FaRegEnvelope className='icon--envelope' />More Info
+        </button>
 
         <div className='candidate-card__exp-wrapper'>
             {Object.entries(experience).map((data, index) => {
@@ -21,6 +32,15 @@ const CandidateCard = ({ key, email, experience }) => {
                 )
             })}
         </div>
+
+        {(hasTransitionedInDetails || showDetails) &&
+            <div className={`candidate-card__details ${hasTransitionedInDetails && 'in'} ${showDetails && 'visible'}`}>
+                <IoClose className='candidate-card__details-close' onClick={() => setShowDetails(false)} />
+                <a href={`mailto:${email}`}>{email}</a>
+                <p>Here will be button for downloading CV</p>
+            </div>
+        }
+        {(hasTransitionedInDetails || showDetails) && <div className={`candidate-card__details-overlay ${hasTransitionedInDetails && 'in'} ${showDetails && 'visible'}`} onClick={() => setShowDetails(false)}></div>}
     </div>
   )
 }
