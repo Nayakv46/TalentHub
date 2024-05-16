b5fe64373f890e8eace4029796ec871adb881fd6
Nayakv46 <bartoszjaniak04@gmail.com>
2024-05-16 15:50:15 +0200

    FormInput text display of skill level on EmployerPage

--- src/components/EmployerPage/FormInput/FormInput.jsx
+++ src/components/EmployerPage/FormInput/FormInput.jsx
@@ -2,6 +2,24 @@ import './formInput.scss';
 import { MdOutlineGrade } from 'react-icons/md';
 
 const FormInput = ({queryData, id, handleInputChange}) => {
+
+    const handleSkillLevel = (level) => {
+        switch (level) {
+            case 1:
+                return 'Intern';
+            case 2:
+                return 'Junior';
+            case 3:
+                return 'Mid';
+            case 4:
+                return 'Senior';
+            case 5:
+                return 'Expert';
+            default:
+                return;
+        }
+    }
+
   return (
     <div className='formInput'>
         <span
@@ -11,7 +29,7 @@ const FormInput = ({queryData, id, handleInputChange}) => {
                 className='formInput__label--icon'
             />
 
-            Skill level
+            Skill level {queryData[id]?.input && `- ${handleSkillLevel(queryData[id]?.input)}` }
         </span>
 
         <div className={`formInput__choice formInput__choice--${queryData[id]?.input}`}>
