ba8c23c465155a212dceeef02a196351fa8aa0ab
Nayakv46 <bartoszjaniak04@gmail.com>
2024-05-28 15:57:12 +0200

    CandidateCardHeader component utilization in CandidateCardDetails

--- src/components/EmployerPage/CandidateCardDetails/CandidateCardDetails.jsx
+++ src/components/EmployerPage/CandidateCardDetails/CandidateCardDetails.jsx
@@ -1,7 +1,7 @@
 import './candidateCardDetails.scss';
 import { IoClose } from 'react-icons/io5';
 import CandidateCardExperience from '../CandidateCardExperience/CandidateCardExperience';
-import defaultUserPic from '../../../assets/UserImage/default-user-pic.png';
+import CandidateCardHeader from '../CandidateCardHeader/CandidateCardHeader';
 import { FaClipboardCheck, FaRegEnvelope } from 'react-icons/fa';
 import { FaClipboardUser } from 'react-icons/fa6';
 import { LuFileBadge } from 'react-icons/lu';
@@ -13,22 +13,7 @@ const CandidateCardDetails = ({ hasTransitionedInDetails, showDetails, email, se
         <div className={`candidateCardDetails ${hasTransitionedInDetails && 'in'} ${showDetails && 'visible'}`}>
             <IoClose className='candidateCardDetails__close' onClick={() => setShowDetails(false)} />
 
-            <div className='candidate-card__header'>
-            <img src={defaultUserPic} alt="profile pic" className='candidate-card__profile-picture'/>
-
-            <div className='candidate-card__info'>
-                <p className='candidate-card__position'>
-                    {position ? position.replace(/\b\w/g, l => l.toUpperCase()) : 'Position not provided'}
-                </p>
-
-                <p className='candidate-card__years'>
-                    {yearsOfExperience == 0 || yearsOfExperience == undefined ? `0 - 1 year of experience` : `${yearsOfExperience}+ years of experience`}
-                </p>
-                <p className='candidate-card__years candidate-card__years--short'>
-                    {yearsOfExperience == 0 || yearsOfExperience == undefined ? `0 - 1 year...` : `${yearsOfExperience}+ years...`}
-                </p>
-            </div>
-        </div>
+            <CandidateCardHeader position={position} yearsOfExperience={yearsOfExperience} />
             <a
                 href={`mailto:${email}`}
                 className='candidateCardDetails__email'
