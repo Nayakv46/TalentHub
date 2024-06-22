426d4b329630dc8aec602387fb3c6dbd666833e7
Nayakv46 <bartoszjaniak04@gmail.com>
2024-06-22 16:27:10 +0200

    EmployerForm SelectSkill component separation with shared file containing styles

--- src/components/EmployerPage/EmployerForm/EmployerForm.jsx
+++ src/components/EmployerPage/EmployerForm/EmployerForm.jsx
@@ -2,14 +2,13 @@ import './employerForm.scss';
 import { useEmployerContext } from '../../../context/EmployerContext';
 import EmployerSubmit from '../EmployerSubmit/EmployerSubmit';
 import CandidateCard from '../CandidateCard/CandidateCard';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import FormSelect from '../FormSelect/FormSelect';
 import FormAddButton from '../FormAddButton/FormAddButton';
 import FormInput from '../FormInput/FormInput';
 import FormRemove from '../FormRemove/FormRemove';
 import FormRange from '../FormRange/FormRange';
-import { PiStackBold } from 'react-icons/pi';
-import { FaChevronDown } from 'react-icons/fa6';
+import SelectSkill from '../SelectSkill/SelectSkill';
 
 const EmployerForm = () => {
 
@@ -17,51 +16,6 @@ const EmployerForm = () => {
 
     const [queryCount, setQueryCount] = useState(1);
     const [showResults, setShowResults] = useState(false);
-
-    const [showOptions, setShowOptions] = useState(false);
-
-    useEffect(() => {
-        const handleDocumentClick = () => {
-          setShowOptions(false);
-        };
-
-        if (showOptions) {
-          document.addEventListener('click', handleDocumentClick);
-        }
-
-        return () => {
-          document.removeEventListener('click', handleDocumentClick);
-        };
-    }, [showOptions]);
-
-    const handleShowOptions = (event) => {
-        event.stopPropagation();
-        setShowOptions(!showOptions);
-    };
-
-    const skillList = [
-        "React",
-        "Vue",
-        "Angular",
-        "Svelte",
-        "Ember",
-        "PHP",
-        "Python",
-        "Ruby",
-        "Java",
-        "C++",
-        "C#",
-        "C",
-        "Swift",
-        "Kotlin",
-        "Javascript",
-        "Typescript",
-        "HTML",
-        "CSS",
-        "SASS",
-        "LESS",
-        "SQL"
-    ]
 
     const generateInputs = () => {
         const inputs = [];
@@ -81,53 +35,11 @@ const EmployerForm = () => {
                         queryData={queryData}
                     />
 
-                    <div className='selectSkill'>
-                        <label
-                            htmlFor={`select-skill-${id}`}
-                            className='selectSkill__label'
-                        >
-                            <PiStackBold
-                                className='selectSkill__label--icon'
-                            />
-                            Select a skill
-                        </label>
-
-                        <div className='selectSkill__inner'>
-                            <div
-                                className={`selectSkill__chosen ${showOptions && `open`}`}
-                                onClick={handleShowOptions}
-                            >
-                                {queryData[id]?.select || "Select"}
-                            </div>
-
-                            <div className='selectSkill__icon'>
-                                <FaChevronDown
-                                    className={`selectSkill__icon-svg ${showOptions && `open`}`}
-                                    onClick={handleShowOptions}
-                                />
-                            </div>
-
-                            <div
-                                className={`selectSkill__options ${showOptions && `open`}`}
-                                id={`select-skill-${id}`}
-                            >
-                                {skillList.map((skill, index) => {
-                                    return (
-                                        <button
-                                            key={`selectSkill-${id}-button-${index}`}
-                                            className='selectSkill__options-button'
-                                            value={skill}
-                                            onClick={(e) => {
-                                                handleSelectChange(id, e.target.value);
-                                            }}
-                                        >
-                                            {skill}
-                                        </button>
-                                    );
-                                })}
-                            </div>
-                        </div>
-                    </div>
+                    <SelectSkill
+                        id={id}
+                        queryData={queryData}
+                        handleSelectChange={handleSelectChange}
+                    />
 
                     <FormInput
                         id={id}
---
+++ src/components/EmployerPage/SelectSkill/SelectSkill.jsx
@@ -0,0 +1,104 @@
+import './selectSkill.scss';
+import { useState, useEffect } from "react";
+import { PiStackBold } from "react-icons/pi";
+import { FaChevronDown } from "react-icons/fa6";
+
+const SelectSkill = ({ queryData, id, handleSelectChange }) => {
+
+    const [showOptions, setShowOptions] = useState(false);
+
+    useEffect(() => {
+        const handleDocumentClick = () => {
+          setShowOptions(false);
+        };
+
+        if (showOptions) {
+          document.addEventListener('click', handleDocumentClick);
+        }
+
+        return () => {
+          document.removeEventListener('click', handleDocumentClick);
+        };
+    }, [showOptions]);
+
+    const handleShowOptions = (event) => {
+        event.stopPropagation();
+        setShowOptions(!showOptions);
+    };
+
+    const skillList = [
+        "React",
+        "Vue",
+        "Angular",
+        "Svelte",
+        "Ember",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Java",
+        "C++",
+        "C#",
+        "C",
+        "Swift",
+        "Kotlin",
+        "Javascript",
+        "Typescript",
+        "HTML",
+        "CSS",
+        "SASS",
+        "LESS",
+        "SQL"
+    ];
+
+  return (
+    <div className='selectSkill'>
+        <label
+            htmlFor={`select-skill-${id}`}
+            className='selectSkill__label'
+        >
+            <PiStackBold
+                className='selectSkill__label--icon'
+            />
+            Select a skill
+        </label>
+
+        <div className='selectSkill__inner'>
+            <div
+                className={`selectSkill__chosen ${showOptions && `open`}`}
+                onClick={handleShowOptions}
+            >
+                {queryData[id]?.select || "Select"}
+            </div>
+
+            <div className='selectSkill__icon'>
+                <FaChevronDown
+                    className={`selectSkill__icon-svg ${showOptions && `open`}`}
+                    onClick={handleShowOptions}
+                />
+            </div>
+
+            <div
+                className={`selectSkill__options ${showOptions && `open`}`}
+                id={`select-skill-${id}`}
+            >
+                {skillList.map((skill, index) => {
+                    return (
+                        <button
+                            key={`selectSkill-${id}-button-${index}`}
+                            className='selectSkill__options-button'
+                            value={skill}
+                            onClick={(e) => {
+                                handleSelectChange(id, e.target.value);
+                            }}
+                        >
+                            {skill}
+                        </button>
+                    );
+                })}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SelectSkill
