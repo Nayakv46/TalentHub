f36347552f19f7a01a9078501809ec389a32455d
Nayakv46 <bartoszjaniak04@gmail.com>
2024-11-20 13:50:15 +0100

    AuthEmployer update of useEffect dependency array

--- src/pages/Auth/Employer/AuthEmployer.jsx
+++ src/pages/Auth/Employer/AuthEmployer.jsx
@@ -42,7 +42,7 @@ const AuthEmployer = () => {
             case 4:
                 return setToggleImage(loginPic5);
         }
-    });
+    }, [userLoggedIn, userType]);
 
   return (
     <div className='auth-employer'>
